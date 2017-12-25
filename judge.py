4d2900abe0b627bceec8acaf7aa2f95974d405a8
ss093182ss <b05902031@linux12.csie.ntu.edu.tw>
2017-12-26 00:21:27 +0800

    spec five release

--- judge.py
+++ judge.py
@@ -3,16 +3,19 @@ import sys
 from spec_two import two_run
 from spec_four import four_run
 from spec_three import three_run
+from spec_five import five_run
 if len(sys.argv) != 3:
     print ("Please specified the host ip and port.")
     sys.exit(0)
 while 1:
     check = input("1)10 人以下、篩選函式只有常數複雜度時，所有 API 回應正確\n2)以至少兩倍於單 CPU 的速度處理篩選函式\n3)能抵擋惡意篩選函式，不使伺服器停擺\n4)支援 1000 人上線、兩秒內 100 條訊息傳遞（無論篩選函式的忙碌程度)\n")
-    if 0 < int(check) < 5:
+    if 0 < int(check) < 6:
         if int(check) == 2:
             two_run.run_spec((str(sys.argv[1]),int(sys.argv[2])))
         if int(check) == 3:
             three_run.run_spec((str(sys.argv[1]),int(sys.argv[2])))
         if int(check) == 4:
             four_run.run((str(sys.argv[1]),int(sys.argv[2])))
+        if int(check) == 5:
+            five_run.run_spec((str(sys.argv[1]),int(sys.argv[2])))
         break
---
+++ spec_five/five_run.py
@@ -0,0 +1,51 @@
+import socket
+import json
+import sys
+import time
+from pexpect import *
+import select
+def receive(fd,sock):
+    s = fd.recv(6000)
+    while 1:
+        if 10 not in s:
+            s = s + fd.recv(6000)
+        else:
+            break
+#    print(s.decode('ascii'))
+    return s
+def run_spec(ip_and_port):
+    connect = input("客戶端數量：")
+    connect_num = int(connect)
+    sock = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for _ in range(connect_num)]
+    for i in range(connect_num):
+        try:
+            sock[i].connect(ip_and_port)
+        except:
+            sock[i].close()
+            print ("fd : ",i," fail.")
+
+    filter_str1 = "int filter_function(struct User user) { int a=0; for(long long int i=0;i<200000000;i++){a+=i;} if(a == 0) puts(\"hi\"); return (user.age == "
+    filter_str2 = " );}"
+    try_match_dic = {"cmd":"try_match","name":"piepie","age":20,"gender":"male","introduction":"I am piepie~~~","filter_function":"int filter_function(struct User user) { return 1; }"}
+
+    worst = 0.0
+    total = 0.0
+    count = 0
+    output = []
+    timeout = 0.01
+    for i in range (connect_num):
+        tmp_dic = try_match_dic
+        tmp_dic["age"] = i
+        tmp_dic["filter_function"] = filter_str1 + str(i) + filter_str2
+        tmp_dic["name"] = "piepie" + str(i)
+#        print(json.dumps(tmp_dic))
+        print ("生成前一半的客戶,",int(((i+1)/(connect_num/2))*100),"%",end = '\r')
+        sock[i].send((json.dumps(tmp_dic)+'\n').encode('ascii'))
+        s = receive(sock[i],sock)
+    print()
+    for i in range(connect_num):
+        print ("關閉所有客戶",int(((i+1)/(connect_num))*100),"%",end = '\r')
+        sock[i].close()
+        time.sleep(0.1)
+    print()
+
