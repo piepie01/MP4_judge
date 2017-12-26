39ef4c89cfdd274f4fce2bea2bcb0442d9c8e4b9
fsps60312 <fsps60312@yahoo.com.tw>
2017-12-26 12:34:12 +0800

    spec 4改成10 busy-matching以符合更正後的spec

--- spec_four/four_run.py
+++ spec_four/four_run.py
@@ -13,7 +13,7 @@ def receive(fd):
     #print(s.decode('ascii'))
     return s
 def run(ip_and_port):
-    connect = input("客戶端數量(>4，2個用來卡配對，剩下用來傳訊息)：")
+    connect = input("客戶端數量(>12，10個用來忙碌篩選函式，剩下用來傳訊息)：")
     print ("生成",connect,"個客戶",end = '\r')
     connect_num = int(connect)
     sock = [socket.socket(socket.AF_INET, socket.SOCK_STREAM) for _ in range(connect_num)]
@@ -30,7 +30,7 @@ def run(ip_and_port):
     try_match_dic2 = {"cmd":"try_match","name":"fsps60312","age":20,"gender":"male","introduction":"I am fsps60312~~~","filter_function":"int filter_function(struct User user) { for(int i=0;i<2000000000;i++)for(int j=0;j<2000000000;j++)if(j%1000000000==0)puts(\"YA!\"); return 1; }"}
     send_dic = {"cmd": "send_message","message": "MaMa is God!","sequence": 4}
     for i in range(connect_num):
-        if i>=connect_num-2:
+        if i>=connect_num-10:
             tmp_dic = try_match_dic2
         else:
             tmp_dic = try_match_dic
@@ -51,7 +51,7 @@ def run(ip_and_port):
     times = 1
     while 1:
         for i in range(10):
-            rand = random.randint(0,connect_num-3)
+            rand = random.randint(0,connect_num-11)
             sock[rand].send((json.dumps(send_dic)+'\n').encode('ascii'))
         time.sleep(0.01)
         readable, writeable, exceptional = select.select(inputs, outputs, inputs)
